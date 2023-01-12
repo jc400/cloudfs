b334e18473f8fc1ed01b687b3087656e7dad2a2a
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-12 13:46:17 -0600

    consolidate resource tests

--- backend/tests/test_file_resources.py
+++ backend/tests/test_file_resources.py
@@ -129,3 +129,34 @@ def test_post_filelist_constraints(app, client, auth):
     auth.login()
     response = client.post("/api/files", json=newfile)
     assert response.status_code == 400
+
+
+def test_get_list(client, auth):
+    auth.login()
+    response = client.get("/api/list/1")
+    assert response.status_code == 200
+    assert b"f1 title" in response.data
+    assert b"d4 title" in response.data
+
+
+def test_get_path(client, auth):
+    auth.login()
+    response = client.get("/api/path/4")
+    assert response.status_code == 200
+    assert b"d4 title" in response.data
+    assert b"Home" in response.data
+
+
+def test_get_starred(client, auth):
+    auth.login()
+    response = client.get("/api/starred")
+    assert response.status_code == 200
+    assert b"d4 title" in response.data
+    assert b"f6 title" in response.data
+    assert b"f5 title" not in response.data
+
+
+def test_get_recent(client, auth):
+    auth.login()
+    response = client.get("/api/recent")
+    assert response.status_code == 200
