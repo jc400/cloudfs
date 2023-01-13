51518d3951da90ac2e4120c791e624c468b5a1d4
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-13 11:53:03 -0600

    add potential data schema, put in config for importing

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,1 +1,18 @@
 export const BACKEND_URL = "/api";
+
+export const template = {
+    "metadata": "",
+    "files": [
+        {
+            "parent": null,
+            "file_type": "d",
+            "title": "home",
+            "content": null,
+            "created": "",
+            "updated": "",
+            "size": "",
+            "starred": false,
+            "tags": []
+        }
+    ]
+}
