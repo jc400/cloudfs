8b20295834cc76aded581b3dbbab84ed55e1b15b
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-13 12:24:55 -0600

    add db state to top level app component, wrap children with context to give them access to db

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { template } from '../../config/config';
 
 import FileExplorer from '../FileExplorer/FileExplorer';
 import Login from '../Login/Login';
@@ -11,38 +12,44 @@ import User from '../User/User';
 
 import './App.css';
 
+
+export const DBContext = createContext();
+
 function App() {
   const [authenticated, setAuthenticated] = useState(null);
+  const [db, setDB] = useState(template);
 
   return (
     <>
-      <BrowserRouter>
-        <User 
-          authenticated={authenticated}
-          setAuthenticated={setAuthenticated}
-        />
-        <Routes>
-          <Route path="/" element={<Redirect url="/file-explorer" />} />
-          <Route path="/login" element={<Login setAuthenticated={setAuthenticated}/>} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/file-explorer"
-            element={
-              <PrivateRoute authenticated={authenticated}>
-                <FileExplorer />
-              </PrivateRoute>
-            }
+      <DBContext.Provider value={{ db, setDB }}>
+        <BrowserRouter>
+          <User
+            authenticated={authenticated}
+            setAuthenticated={setAuthenticated}
           />
-          <Route 
-            path="/text-editor/:file_id" 
-            element={
-              <PrivateRoute authenticated={authenticated}>
-                <TextEditor />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Redirect url="/file-explorer" />} />
+            <Route path="/login" element={<Login setAuthenticated={setAuthenticated} />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/file-explorer"
+              element={
+                <PrivateRoute authenticated={authenticated}>
+                  <FileExplorer />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/text-editor/:file_id"
+              element={
+                <PrivateRoute authenticated={authenticated}>
+                  <TextEditor />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </DBContext.Provider>
     </>
   );
 }
