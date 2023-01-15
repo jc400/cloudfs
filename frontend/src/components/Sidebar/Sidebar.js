54efba84d84d2ad75a5156820a2a4afd0c4bde1d
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-15 16:14:11 -0600

    initial stab at sidebar component

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,14 +1,49 @@
 import React from 'react';
-import './Sidebar.css';
 
-export default function Sidebar({show, children}) {
+import Button from '../Button/Button';
+import Icon from '../Icon/Icon';
+
+import './Sidebar.css';
+import folder from '../../assets/folder.svg';
+import search from '../../assets/search.svg';
+import star from '../../assets/empty_star.png';
+import settings from '../../assets/settings.svg';
+
+export default function Sidebar({activeMid, setActiveMid}) {
+
+    const bg = {backgroundColor: "var(--gray1)"};
     return (
-        <>
-            {show && (
-                <div id="sidebar">
-                    {children}
-                </div>
-            )}
-        </>
+        <div id="Sidebar">
+            <ul>
+                <li>
+                    <Button
+                        onClick={() => setActiveMid("FileExplorer")}
+                        tooltip="Show the File Explorer"
+                        style={bg}
+                    ><Icon src={folder} /></Button>
+                </li>
+                <li>
+                    <Button
+                        onClick={() => setActiveMid("Search")}
+                        tooltip="Search"
+                        style={bg}
+                    ><Icon src={search} /></Button>
+                </li>
+                <li>
+                    <Button
+                        onClick={() => setActiveMid("Starred")}
+                        tooltip="Show starred files"
+                        style={bg}
+                    ><Icon src={star} /></Button>
+                </li>
+                <li>
+                    <Button
+                        onClick={() => setActiveMid("Settings")}
+                        tooltip="Show settings"
+                        style={bg}
+                    ><Icon src={settings} /></Button>
+                </li>
+            </ul>
+        </div>
     )
 }
