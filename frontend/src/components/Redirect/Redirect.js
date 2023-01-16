40d72a7c3a0ffdea0ad0054d57aa718f3c272bb7
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-16 03:28:50 -0600

    remove components I expect not to use

--- frontend/src/components/Redirect/Redirect.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-export default function Redirect({ url }){
-    const navigate = useNavigate();
-    useEffect(() => navigate(url), []);
-    return <></>;
-}
