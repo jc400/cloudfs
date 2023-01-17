b3b6a0c9c9b1eb190e1092e3e1cc7f67804279c1
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-16 22:01:22 -0600

    style search

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -7,41 +7,43 @@ import ScrollArea from '../ScrollArea/ScrollArea';
 
 import './Search.css';
 
+
 export default function Search({ activeMid, setActiveFile }) {
     const { db } = useContext(DBContext);
     const [ query, setQuery] = useState(null);
-
-    const search = () => {
-        return Object.entries(db.files).filter(([k, v]) => {
-            return v.title?.includes(query) || v.content?.includes(query) || v.tags?.includes(query);
-        })
-    }
-    const handleSearchChange = ev => setQuery(ev.target.value);
+    const handleQueryChange = ev => setQuery(ev.target.value);
 
     const FileCallbacks = {
         handleClick: (ev, file_key) => setActiveFile(file_key),
     }
+
+
+    // get search results
+    const results = Object.entries(db.files)
+        .filter(([k, v]) =>  v.title?.includes(query) || v.content?.includes(query) || v.tags?.includes(query))
+        .map(([k, v]) => (
+            <File
+                key={k}
+                file={v}
+                file_key={k}
+                callbacks={FileCallbacks}
+            />
+        ));
+    
 
     return (
         <>
             {activeMid === "Search" &&
                 <div className="Search">
                     <div className="Search-header">
-                        Search
-                        <input value={query || ''} onChange={handleSearchChange} />
+                        <span>SEARCH</span>
+                        <input value={query || ''} onChange={handleQueryChange} />
                     </div>
+
                     <ScrollArea bgColor="var(--gray3)">
-                        {search().map(([k, v]) => (
-                            <File
-                                key={k}
-                                file={v}
-                                file_key={k}
-                                callbacks={FileCallbacks}
-                                columns={{ name: true, size: false, updated: false, starred: false }}
-                                style={{ fontSize: '0.85em', border: 'none' }}
-                            />
-                        ))}
+                        {results}
                     </ScrollArea>
+
                 </div>
             }
         </>
