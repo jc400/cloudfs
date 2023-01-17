c4719f0bef6f06ff813d665ec07df479982aee64
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-16 22:04:48 -0600

    style starred

--- frontend/src/components/Starred/Starred.js
+++ frontend/src/components/Starred/Starred.js
@@ -8,30 +8,29 @@ import './Starred.css';
 
 export default function Starred({ activeMid, setActiveFile }) {
     const { db } = useContext(DBContext);
-
-    const starredFiles = Object.entries(db.files)
-        .filter(([k, v]) => v.starred);
-
     const FileCallbacks = {
         handleClick: (ev, file_key) => setActiveFile(file_key),
     }
+
+
+    const starredFiles = Object.entries(db.files)
+        .filter(([k, v]) => v.starred)
+        .map(([k, v]) => (
+            <File
+                key={k}
+                file={v}
+                file_key={k}
+                callbacks={FileCallbacks}
+            />
+        ))
 
     return (
         <>
             {activeMid === "Starred" &&
                 <div className="Starred">
-                    <div className="Starred-header">Starred</div>
+                    <div className="Starred-header">STARRED</div>
                     <ScrollArea bgColor="var(--gray3)">
-                        {starredFiles.map(([k, v]) => (
-                            <File
-                                key={k}
-                                file={v}
-                                file_key={k}
-                                callbacks={FileCallbacks}
-                                columns={{ name: true, size: false, updated: false, starred: false }}
-                                style={{ fontSize: '0.85em', border: 'none' }}
-                            />
-                        ))}
+                        {starredFiles}
                     </ScrollArea>
                 </div>
             }
