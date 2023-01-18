1554d7a511f49b82725c491c8b61e4b1c2009c71
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-18 15:22:26 -0600

    remove tooltip component from iconbutton, turns out browser does tooltips automatically if you use a title attr

--- frontend/src/components/IconButton/IconButton.js
+++ frontend/src/components/IconButton/IconButton.js
@@ -1,22 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
-import Tooltip from '../Tooltip/Tooltip';
 
 import './IconButton.css';
 
 export default function IconButton({src, size, onClick, tooltip}){
-    const [showTooltip, setShowTooltip] = useState(false);
 
     return (
         <button 
+            title={tooltip}
             className="IconButton"
             onClick={onClick} 
-            onMouseEnter={()=>setShowTooltip(true)} 
-            onMouseLeave={()=>setShowTooltip(false)}
         >
             <img src={src} width={size} />
-            {tooltip && <Tooltip show={showTooltip} text={tooltip} />}
         </button>
     )
 }
