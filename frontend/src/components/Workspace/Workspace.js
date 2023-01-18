43e99334828290ed5fe090a6cf68408bb788e8e5
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-18 15:09:45 -0600

    refactor workspace tabs with bootstrap

--- frontend/src/components/Workspace/Workspace.js
+++ frontend/src/components/Workspace/Workspace.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { DBContext } from '../App/App';
-import displayDate from '../../services/displayDate';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Nav from 'react-bootstrap/Nav';
 
 import Tag from '../Tag/Tag';
 import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
@@ -79,53 +83,68 @@ export default function TextEditor({ UIState }) {
 
     return (
         <div className="WS">
-            <div className="WS-tabs">
-                {tabs.map(file_key => (
-                    <span 
-                        key={file_key} 
-                        id={file_key === UIState.activeFile ? "WS-tab-active" : null}
-                        onClick={() => openTab(file_key)}    
-                    >
-                        {db.files[file_key].title}
-                        &nbsp;
-                        <IconButton 
-                            src={CloseIcon}
-                            size={"15px"}
-                            onClick={ev => {ev.stopPropagation(); removeTab(file_key)}}
-                        />
-                    </span>
-                ))}
-            </div>
+            <Tab.Container
+                id="WS-tab-container"
+                activeKey={UIState.activeFile}
+                onSelect={k => openTab(k)}
+            >
 
-            <div className="WS-fileinfo">
-                <span className="WS-fileinfo-breadcrumbs">
-                    <Breadcrumbs 
-                        file_key={UIState.activeFile}
-                    ></Breadcrumbs>
-                </span>
-                <span className="WS-fileinfo-tags">
-                    {db.files[UIState.activeFile]?.tags.map(tag => (
-                        <Tag 
-                            key={tag} 
-                            name={tag} 
-                            onClick={() => TagSearch(tag)}
-                            remove={()=>removeTag(tag)} 
-                        />
+                <Nav className="WS-tabs">
+                    {tabs.map(file_key => (
+                        <Nav.Link
+                            key={file_key}
+                            eventKey={file_key}
+                            title={db.files[file_key].title}
+                            className="WS-tab-item text-reset"
+                        >
+                            {db.files[file_key].title}
+                            &nbsp;
+                            <IconButton 
+                                src={CloseIcon}
+                                size={"15px"}
+                                onClick={ev => {ev.stopPropagation(); removeTab(file_key)}}
+                            />
+                        </Nav.Link>
                     ))}
-                    <Tag onClick={addTag} name="+New" />
-                </span>
-            </div>
-            
-            <ScrollArea bgColor="var(--gray2)">
-                <textarea
-                    id="WS-content"
-                    name="WS-content"
-                    cols="50"
-                    rows={document?.content.split('').filter(c => c === '\n').length + 10}
-                    value={document?.content || ''}
-                    onChange={handleContentChange}
-                ></textarea>
-            </ScrollArea>
+                </Nav>
+
+                <Tab.Content className="overflow-hidden">
+                    {tabs.map(file_key => (
+                        <Tab.Pane eventKey={file_key}>
+                            <div className="WS-fileinfo">
+                                <span className="WS-fileinfo-breadcrumbs">
+                                    <Breadcrumbs 
+                                        file_key={file_key}
+                                    ></Breadcrumbs>
+                                </span>
+                                <span className="WS-fileinfo-tags">
+                                    {db.files[file_key]?.tags.map(tag => (
+                                        <Tag 
+                                            key={tag} 
+                                            name={tag} 
+                                            onClick={() => TagSearch(tag)}
+                                            remove={()=>removeTag(tag)} 
+                                        />
+                                    ))}
+                                    <Tag onClick={addTag} name="+New" />
+                                </span>
+                            </div>
+                            
+                            <ScrollArea bgColor="var(--gray2)">
+                                <textarea
+                                    className="WS-content"
+                                    name="WS-content"
+                                    cols="50"
+                                    rows={document?.content.split('').filter(c => c === '\n').length + 10}
+                                    value={document?.content || ''}
+                                    onChange={handleContentChange}
+                                ></textarea>
+                            </ScrollArea>
+                        </Tab.Pane>
+                    ))}
+                </Tab.Content>
+
+            </Tab.Container>
         </div>
     )
 }
