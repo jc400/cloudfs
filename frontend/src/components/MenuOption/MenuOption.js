558478e7eda818ca78570d249e6c3f609d376442
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-19 11:31:43 -0600

    allow disabling a menuoption

--- frontend/src/components/MenuOption/MenuOption.js
+++ frontend/src/components/MenuOption/MenuOption.js
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './MenuOption.css';
 
-export default function MenuOption({name, onClick, style}){
+export default function MenuOption({name, onClick, disabled, style}){
     return (
-        <button onClick={onClick} className="MenuOption" style={style}>
-            {name}
-        </button>
+        <>
+            {
+            disabled
+            ? <button onClick={onClick} className="MenuOption" style={style} disabled>{name}</button>
+            : <button onClick={onClick} className="MenuOption" style={style}>{name}</button>
+            }
+        
+        </>
     )
 }
 
@@ -14,4 +19,8 @@ MenuOption.propTypes = {
     name: PropTypes.string.isRequired,
     onClick: PropTypes.func,
     style: PropTypes.object,
+    disabled: PropTypes.bool,
+}
+MenuOption.defaultProps = {
+    disabled: false
 }
