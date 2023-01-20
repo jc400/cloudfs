640cdb4947e82ed0a82fe2aaa651ecbf41c5520b
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-20 10:43:54 -0600

    consolidate auth and api into single service file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,28 @@
 import hit from './hit';
 
+export async function register(username, password){
+    return hit("POST", "auth/register", {
+        username: username,
+        password: password,
+    });
+} 
+
+export async function login(username, password){
+    return hit("POST", "auth/login", {
+        username: username,
+        password: password,
+    });
+} 
+
+export async function logout(){
+    return hit("GET", "auth/logout");
+} 
+
+export async function check_login(){
+    return hit("GET", "auth/check_login");
+}
+
+
 export async function getBlob() {
     const response = await hit("GET", "blob");
     const encrypted = response?.blob;
