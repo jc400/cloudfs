d605415881695e9f88ae5f4eb23c0c040d236c06
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-22 17:44:19 -0600

    we want to position based on window size, not total screen size

--- frontend/src/services/getPositionStyle.js
+++ frontend/src/services/getPositionStyle.js
@@ -1,13 +1,13 @@
 export default function getPositionStyle(x, y) {
     // returns X/Y coords plus style rules to ensure div stays on screen. For context menu, etc
     let style = {}
-    if (x > window.screen.availWidth / 2) {
+    if (x > window.innerWidth / 2) {
         style['right'] = 0;
     } else {
         style['left'] = 0;
     }
 
-    if (y > window.screen.availHeight / 2) {
+    if (y > window.innerHeight / 2) {
         style['bottom'] = 'var(--button-height)';
     } else {
         style['top'] = 'var(--button-height)';
