47776f235d75e9cafb2a13c9b115f2a477d9fe6f
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-29 15:11:39 -0600

    change queries for postgres & pg-promise

--- backend-express/routes/auth.js
+++ backend-express/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { get_hashed_password, check_hashed_password } = require('../auth_utils');
-const { get_db } = require('../db.js');
+const db = require('../db.js');
 
 
 const router = express.Router();
@@ -10,21 +10,21 @@ router.post('/register', function (req, res, next) {
     const username = req?.body?.username;
     const password = req?.body?.password;
     const vault = req?.body?.vault;
-    const dbh = get_db();
 
     if (username && password && vault) {
         const {hashed_password, salt} = get_hashed_password(password);
-        dbh.run(
-            "INSERT INTO users (username, hashed_password, salt, vault) VALUES (?, ?, ?, ?)",
-            [username, hashed_password, salt, vault],
-            function (err) {
-                if (err === null) {
-                    res.send({ success: true, message: "Registered" });
-                } else {
-                    res.send({ success: false, message: err.message });
-                }
-            }
+
+        db.none(
+            "INSERT INTO users(username, hashed_password, salt, vault) VALUES($1, $2, $3, $4)",
+            [username, hashed_password, salt, vault]
         )
+        .then(() => {
+            res.send({ success: true, message: "Registered" });
+        })
+        .catch(err => {
+            res.send({ success: false, message: err.message });
+        });
+
     } else {
         res.send({ success: false, message: "Something went wrong" });
     }
@@ -33,31 +33,31 @@ router.post('/register', function (req, res, next) {
 router.post('/login', function (req, res, next) {
     const username = req?.body?.username;
     const password = req?.body?.password;
-    const dbh = get_db();
+
     if (username && password) {
-        dbh.get(
-            "SELECT * FROM users WHERE username = ?",
+        db.one(
+            "SELECT * FROM users WHERE username = $1",
             [username],
-            function (err, row) {
-                if (err === null) {
-                    if (row?.user_id){
-                        if (check_hashed_password(password, row.hashed_password, row.salt)) {
-                            req.session.regenerate(function () {
-                                // if everything checks out, generate a session
-                                req.session.user_id = row.user_id;
-                                res.send({ success: true, message: "Logged in" });
-                            });
-                        } else {
-                            res.send({ success: false, message: "Incorrect credentials"})
-                        }
-                    } else {
-                        res.send({ success: false, message: "Incorrect credentials"});
-                    }
-                } else {
-                    res.send({ success: false, message: err.message });
-                }
-            }
         )
+        .then(row => {
+            if (row?.user_id){
+                if (check_hashed_password(password, row.hashed_password, row.salt)) {
+                    req.session.regenerate(function () {
+                        // if everything checks out, generate a session
+                        req.session.user_id = row.user_id;
+                        res.send({ success: true, message: "Logged in" });
+                    });
+                } else {
+                    res.send({ success: false, message: "Incorrect credentials"})
+                }
+            } else {
+                res.send({ success: false, message: "Incorrect credentials"});
+            }
+        })
+        .catch(err => {
+            res.send({ success: false, message: err.message });
+        });
+
     } else {
         res.send({ success: false, message: "missinge user/pass" });
     }
