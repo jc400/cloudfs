da0e4dec1d57bbc3067cece84aca647c0597a0b3
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-29 12:08:09 -0600

    refactor to use bytes directly (instead of storing as hex)

--- backend-flask/app/auth_utils.py
+++ backend-flask/app/auth_utils.py
@@ -8,23 +8,39 @@ PASSWORD_KEY_LENGTH = 64
 PBKDF2_DIGEST_ALGO = 'sha256'
 
 def _hash(password, salt):
-    """Accepts password/salt as hex strings, returns hash as hex value"""
-    password = password.encode('utf-8')
-    salt = salt.encode('utf-8')
+    """Accepts password/salt as byte strings, returns hash as bytes"""
     return hashlib.pbkdf2_hmac(
         PBKDF2_DIGEST_ALGO, 
         password, 
         salt, 
         PBKDF2_ITERATIONS
-    ).hex()
+    )
+
 
 def get_hashed_password(password):
-    """Accepts password as string, returns hex password hash + salt"""
-    salt = secrets.token_bytes(SALT_LENGTH).hex()
+    """Accepts password as string, returns password hash + salt as bytes"""
+
+    # generate random salt bytes
+    salt = secrets.token_bytes(SALT_LENGTH)
+
+    # convert string to byte encoding of that string
+    password = password.encode('utf-8')
+
+    # calculate hash from password & salt bytes
     hashed_password = _hash(password, salt)
+
+    # return both, still bytes
     return hashed_password, salt
+
 
 def check_hashed_password(password_attempt, hashed_password_from_db, salt_from_db):
     """Accepts password attempt as string, hashed_password and salt as hex. Returns bool"""
+    
+    # convert to byte encoding of string
+    password_attempt = password_attempt.encode('utf-8')
+    
+    # calculate hash of attempt from passwrod/salt bytes
     hashed_attempt = _hash(password_attempt, salt_from_db)
+
+    # compare
     return secrets.compare_digest(hashed_attempt, hashed_password_from_db)
