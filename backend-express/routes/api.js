b96715092e99b0bb57d97a4558dfd16b2c4c4f0b
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-29 15:14:32 -0600

    update api to use postgres

--- backend-express/routes/api.js
+++ backend-express/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { login_required } = require('../auth_utils');
-const { get_db } = require('../db.js');
+const db = require('../db.js');
 
 
 const router = express.Router();
@@ -8,36 +8,36 @@ const router = express.Router();
 
 router.get('/vault', login_required, function (req, res, next){
     const user_id = req.session.user_id;
-    const dbh = get_db();
-    dbh.get(
-        "SELECT vault FROM users WHERE user_id = ?",
-        user_id,
-        function (err, row) {
-            if (err === null){
-                res.send({vault: row.vault});
-            } else {
-                res.send(err.message);
-            }
-        }
+
+    db.one(
+        "SELECT vault FROM users WHERE user_id = $1",
+        [user_id]
     )
+    .then(row => {
+        res.send({vault: row.vault});
+    })
+    .catch(err => {
+        res.send(err.message);
+    });
+
 })
 
 router.put('/vault', login_required, function (req, res, next){
     const vault = req?.body?.vault;
     const user_id = req.session.user_id;
-    const dbh = get_db();
+
     if (user_id && vault) {
-        dbh.run(
-            "UPDATE users SET vault = ? WHERE user_id = ?",
-            [vault, user_id],
-            function (e) {
-                if (e === null) {
-                    res.send({ success: true, message: "" });
-                } else {
-                    res.send({ success: false, message: e.message });
-                }
-            }
+        db.none(
+            "UPDATE users SET vault = $1 WHERE user_id = $2",
+            [vault, user_id]
         )
+        .then(() => {
+            res.send({ success: true, message: "" });
+        })
+        .catch(err => {
+            res.send({ success: false, message: err.message });
+        });
+
     } else {
         res.send({ success: false, message: "Something went wrong" });
     }
