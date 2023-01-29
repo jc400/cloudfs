e47da95852056d9ae1a2f3b45c16cdc4f7b441b2
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-29 15:11:01 -0600

    connect to postgres instead of sqlite. Remove init_db script, this will live elsewhere

--- backend-express/db.js
+++ backend-express/db.js
@@ -1,24 +1,4 @@
-const sqlite3 = require('sqlite3').verbose()
-const { readFile } = require('fs/promises')
-const path = require('path')
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://postgres:password@db/app')
 
-
-async function init_db() {
-    const schema = await readFile(path.join(__dirname, 'schema.sql'), 'utf8');
-    const db = get_db();
-
-    db.serialize(() => {
-        db.exec(schema, function(e){console.log(e)});
-    });
-    db.close();
-
-    console.log('initialized database');
-}
-
-function get_db() {
-    const db = new sqlite3.Database(path.join(__dirname, 'instance', 'db.sqlite'), sqlite3.OPEN_READWRITE);
-    return db;
-}
-
-exports.init_db = init_db;
-exports.get_db = get_db;
+module.exports = db;
