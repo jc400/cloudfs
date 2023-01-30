294bc005ae43ca9482ee03f5ac4c953565284113
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-30 15:00:12 -0600

    remove unused db files

--- backend-flask/app/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username VARCHAR UNIQUE NOT NULL,
-    salt VARCHAR, 
-    hashed_password VARCHAR,
-    vault VARCHAR
-);
