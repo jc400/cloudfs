3f92130333f97f953d8e154b7d77dd3048ac50d8
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-30 13:32:09 -0600

    db reads database dsn from current_app.config

--- backend-flask/app/db.py
+++ backend-flask/app/db.py
@@ -1,11 +1,10 @@
 import psycopg2
 from flask import current_app, g
-import os
 
 
 def get_db():
     if "dbh" not in g:
-        g.dbh = psycopg2.connect(os.environ['POSTGRES_DSN'])
+        g.dbh = psycopg2.connect(current_app.config['DATABASE'],)
     return g.dbh
 
 
