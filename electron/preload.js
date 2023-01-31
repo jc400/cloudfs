1e4d65cf31fb2f3c5320af7dea8223e6eb517d0f
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-31 15:17:40 -0600

    remove preload stuff

--- electron/preload.js
+++ electron/preload.js
@@ -1,12 +1,3 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    // loops through deps in main window and replaces with correct version
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
-
-    for (const dependency of ['chrome', 'node', 'electron']) {
-        replaceText(`${dependency}-version`, process.versions[dependency])
-    }
 })
