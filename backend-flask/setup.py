dc841380681ec66f47e98619866c997a90186dd4
jc400 <105320127+jc400@users.noreply.github.com>
2023-02-08 12:26:23 -0600

    add flask-jwt-extended dependency

--- backend-flask/setup.py
+++ backend-flask/setup.py
@@ -6,6 +6,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'flask', 'flask-restful'
+        'flask', 
+        'flask-restful',
+        'Flask-JWT-Extended'
     ],
 )
