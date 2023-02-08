b94ae27b878ab571fcaee157e42e7901b9fc5229
jc400 <105320127+jc400@users.noreply.github.com>
2023-02-08 12:23:20 -0600

    add token_blocklist table

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS token_blocklist;
 
 CREATE TABLE users (
     user_id BIGSERIAL PRIMARY KEY,
@@ -7,3 +8,10 @@ CREATE TABLE users (
     hashed_password VARCHAR,
     vault VARCHAR
 );
+
+CREATE TABLE token_blocklist (
+    token_id BIGSERIAL PRIMARY KEY,
+    jti CHAR(36) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+CREATE INDEX tok_jti_idx ON token_blocklist (jti);
