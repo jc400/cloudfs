59c8dc493b3de6f631e4d06ba560aeb0e3e13adf
jc400 <105320127+jc400@users.noreply.github.com>
2023-02-08 14:47:25 -0600

    refactor to send authorization header + token with relevant requests. also remove dependency on hit abstraction

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,36 +1,137 @@
-import hit from './hit';
+import { BACKEND_URL } from '../config/config';
 
 export async function register(username, password, vault){
-    return hit("POST", "auth/register", {
-        username: username,
-        password: password,
-        vault: vault,
-    });
-} 
+    const url = `${BACKEND_URL}/auth/register`;
+    const options = {
+        method: 'POST',
+        mode: 'cors',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            username: username,
+            password: password,
+            vault: vault,
+        })
+    }
 
-export async function login(username, password){
-    return hit("POST", "auth/login", {
-        username: username,
-        password: password,
-    });
-} 
-
-export async function logout(){
-    return hit("GET", "auth/logout");
-} 
-
-export async function check_login(){
-    return hit("GET", "auth/check_login");
+    return fetch(url, options)
+    .then(resp => {
+        if (resp?.ok) {
+            return resp;
+        } else {
+            throw resp; 
+        }
+    })
+    .then(resp => resp.json())
 }
 
+export async function login(username, password){
+    const url = `${BACKEND_URL}/auth/login`;
+    const options = {
+        method: 'POST',
+        mode: 'cors',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        })
+    }
 
-export async function getVault() {
-    return hit("GET", "vault")
+    return fetch(url, options)
+    .then(resp => {
+        if (resp?.ok) {
+            return resp;
+        } else {
+            throw resp; 
+        }
+    })
+    .then(resp => resp.json())
+}
+
+export async function logout(token){
+    const url = `${BACKEND_URL}/auth/logout`;
+    const options = {
+        method: 'DELETE',
+        mode: 'cors',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    }
+
+    return fetch(url, options)
+    .then(resp => {
+        if (resp?.ok) {
+            return resp;
+        } else {
+            throw resp; 
+        }
+    })
+    .then(resp => resp.json())
+}
+
+export async function check_login(token){
+    const url = `${BACKEND_URL}/auth/check_login`;
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    }
+
+    return fetch(url, options)
+    .then(resp => {
+        if (resp?.ok) {
+            return resp;
+        } else {
+            throw resp; 
+        }
+    })
+    .then(resp => resp.json())
+}
+
+export async function getVault(token) {
+    const url = `${BACKEND_URL}/vault`;
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    }
+
+    return fetch(url, options)
+    .then(resp => {
+        if (resp?.ok) {
+            return resp;
+        } else {
+            throw resp; 
+        }
+    })
+    .then(resp => resp.json())
     .then(resp => resp.vault);
 }
 
-export async function putVault(vault) {
-    return hit("PUT", "vault", {
-        vault: vault
-    });
+export async function putVault(vault, token) {
+    const url = `${BACKEND_URL}/vault`;
+    const options = {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            vault: vault
+        })
+    }
+
+    return fetch(url, options)
+    .then(resp => {
+        if (resp?.ok) {
+            return resp;
+        } else {
+            throw resp; 
+        }
+    })
+    .then(resp => resp.json())
 }
