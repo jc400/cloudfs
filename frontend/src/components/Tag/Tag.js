4a594930e2ccaed1d5aa029e58a209fcfdedd94b
jc400 <105320127+jc400@users.noreply.github.com>
2023-02-09 10:44:07 -0600

    formatting

--- frontend/src/components/Tag/Tag.js
+++ frontend/src/components/Tag/Tag.js
@@ -1,22 +1,20 @@
 import React from 'react';
-
 import IconButton from '../IconButton/IconButton';
-
 import './Tag.css';
 import CloseIcon from '../../assets/x.svg';
 
-export default function Tag({name, onClick, remove}){
+
+export default function Tag({ name, onClick, remove }) {
     return (
         <div className="tag" onClick={onClick}>
             <span>{name}</span>
-            {remove && 
-                <IconButton 
+            {remove &&
+                <IconButton
                     src={CloseIcon}
                     size={"15px"}
-                    onClick={ev => {ev.stopPropagation(); remove()}}
+                    onClick={ev => { ev.stopPropagation(); remove() }}
                 />
             }
-            
-</div>
+        </div>
     )
 }
