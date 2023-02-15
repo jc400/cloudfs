c5355733321ace2960b9f2357995a5d71f3833b0
jc400 <105320127+jc400@users.noreply.github.com>
2023-02-15 16:25:56 -0600

    pull out rename form to its own const

--- frontend/src/components/File/File.js
+++ frontend/src/components/File/File.js
@@ -11,6 +11,7 @@ export default function File({ file, file_key, callbacks, selected, to_rename })
     const [newName, setNewName] = useState(file?.title);
     const inputRef = useRef();
 
+    // event handlers
     const handleSubmit = ev => {
         // renames file (if not blank) and closes rename form
         ev.preventDefault();
@@ -20,60 +21,61 @@ export default function File({ file, file_key, callbacks, selected, to_rename })
         callbacks.close_rename();
     }
     const handleKeydown = ev => {
-        if (ev.key === 'Enter'){
+        if (ev.key === 'Enter') {
             callbacks.open(file_key);
         }
     }
 
+    // focus rename form immediately
     useEffect(() => {
-        // focus() input when form is shown
-        if (to_rename){
+        if (to_rename) {
             inputRef.current.focus();
         }
     }, [to_rename]);
 
+    // different HTML if File is being renamed
+    const renameForm = (
+        <li className="File" role="treeitem" tabIndex="0">
+            <span>
+                <form id="rename" name="rename" onSubmit={handleSubmit}>
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        id="filename"
+                        name="filename"
+                        aria-label="New file name"
+                        size="10"
+                        value={newName}
+                        onChange={ev => setNewName(ev.target.value)}
+                        onBlur={() => { setNewName(''); callbacks.close_rename() }}
+                    />
+                </form>
+            </span>
+        </li>
+    );
+    const FileHTML = (
+        <li
+            className="File"
+            role="treeitem"
+            tabIndex="0"
+            onFocus={() => callbacks.select(file_key)}
+            aria-selected={selected ? "true" : "false"}
+            onKeyDown={handleKeydown}
+            onDoubleClick={() => callbacks.open(file_key)}
+            onContextMenu={ev => callbacks.openContextMenu(ev, file_key)}
+            style={selected ? { backgroundColor: 'var(--accent)' } : {}}
+        >
+            <Icon src={FileIcon} size="14px" />
+            &nbsp;
+            <span>{file?.title}</span>
+        </li>
+    )
+
 
     return (
         <>
-            {to_rename
-                ? 
-                <li className="File" role="treeitem" tabIndex="0"> 
-                    <span>
-                        <form id="rename" name="rename" onSubmit={handleSubmit}>
-                            <input 
-                                ref={inputRef}
-                                type="text" 
-                                id="filename"
-                                name="filename"
-                                aria-label="New file name"
-                                size="10"
-                                value={newName}
-                                onChange={ev => setNewName(ev.target.value)}
-                                onBlur={() => {setNewName(''); callbacks.close_rename()}}
-                            />
-                        </form>
-                    </span>
-                </li> 
-                : 
-                <li
-                    className="File"
-                    role="treeitem"
-                    tabIndex="0"
-                    onFocus={() => callbacks.select(file_key)}
-                    aria-selected={selected ? "true" : "false"}
-                    onKeyDown={handleKeydown}
-                    onDoubleClick={() => callbacks.open(file_key)}
-                    onContextMenu={ev => callbacks.openContextMenu(ev, file_key)}
-                    style={selected ? { backgroundColor: 'var(--accent)' } : {}}
-                >
-                    <Icon src={FileIcon} size="14px" />
-                    &nbsp;
-                    <span>{file?.title}</span>
-                </li>
-            }
-
+            {to_rename ? renameForm : FileHTML}
         </>
-
     )
 }
 
